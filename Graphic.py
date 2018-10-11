41b0730b297ddbb2cf4375fb01636471a9276ebb
Nathaly <natharb09@gmail.com>
2018-10-10 22:38:43 -0500

    Iniciando con Python

---
+++ Graphic.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+square = turtle.Turtle()
+
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+
+turtle.mainloop()
